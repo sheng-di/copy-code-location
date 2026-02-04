96af39a79ce357a70e5ff1189cb80e2af78e0610
shengdi <378978764@qq.com>
2026-02-04 12:49:21 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# copy-code-and-filename
+
+Copy code with automatic code-block formatter including filename and file extension.
+Optimized for quickly sharing code with LLMs whereby they easily recognize the language of code in the file.
+
+## Features
+
+_Transform this:_
+const a;
+
+_to this:_
+```filename.js
+const a;
+```
+
+_with adding template literals and new lines:_
+\`\`\`\n${copiedContent}\n\`\`\`
+
+### Default keybinding
+
+- Windows: ```Ctrl+Alt+C```
+- Mac: ```Cmd+Shift+F9```
+
+**Enjoy!**
---
+++ extension.js
@@ -0,0 +1,35 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+	const disposable = vscode.commands.registerCommand('copy-code-and-filename.copycode', () => {
+
+		// Get the full file path using fsPath (as we discussed earlier)
+		const filePath = vscode.window.activeTextEditor.document.uri.fsPath;
+
+		// Extract the filename with extension using 'path.basename'
+        const fileNameWithExtension = require('path').basename(filePath);
+
+		// Regular copy, then edit clipboard
+		vscode.commands.executeCommand('editor.action.clipboardCopyAction').then(async () => {
+			const clipboardContent = await vscode.env.clipboard.readText();
+
+			vscode.env.clipboard.writeText(`\`\`\`${fileNameWithExtension}\n${clipboardContent}\n\`\`\``);
+		});
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+//exports.activate = activate;
+
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+};
